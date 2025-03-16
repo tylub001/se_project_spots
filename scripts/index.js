0f136a8cb3cdb9643afd92412248c841ef7fd3c5
Brittany Tylutke <tylub001@gmail.com>
2025-03-16 18:07:13 -0400

    sprint 4 project 4 before template

--- scripts/index.js
+++ scripts/index.js
@@ -26,19 +26,35 @@ const initialCards = [
 ];
 
 const profileEditBtn = document.querySelector(".profile__edit-btn");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+
 const editModal = document.querySelector("#edit-profile-modal");
+const editFormElement = editModal.querySelector(".modal__form")
 const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
+const editModalNameInput = editModal.querySelector("#profile-name-input");
+const editModalDescriptionInput = editModal.querySelector("#profile-description-input");
 
 
 function openModal() {
+  editModalNameInput.value = profileName.textContent;
+  editModalDescriptionInput.value = profileDescription.textContent;
   editModal.classList.add("modal_opened");
 }
 
 function closeModal() {
   editModal.classList.remove("modal_opened");
 }
+function handleEditFormSubmit(evt) {
+evt.preventDefault();
+profileName.textContent = editModalNameInput.value;
+profileDescription.textContent = editModalDescriptionInput.value;
+closeModal();
+}
 
 profileEditBtn.addEventListener("click", openModal);
-
 editModalCloseBtn.addEventListener("click", closeModal);
+editFormElement.addEventListener("submit", handleEditFormSubmit);
+
+
 
